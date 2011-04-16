0115331f6781de5adbbd25e25775d8970db5a2a1
Andrew Ferguson <andrew@128-8-211-72.wireless.umd.edu>
2011-04-16 17:11:27 -0400

    Just created a simple Hello World! splash screen.

--- HelloWorldSample/HelloWorldSampleAppDelegate.m
+++ HelloWorldSample/HelloWorldSampleAppDelegate.m
@@ -21,7 +21,7 @@
 {
     // Override point for customization after application launch.
      
-    self.window.rootViewController = self.viewController;
+    //self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
 }
